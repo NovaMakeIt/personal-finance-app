14bbef95f89839c5b233ec96a30e3b3f3bb1565e
NOVA <147254516+NovaMakeIt@users.noreply.github.com>
2024-10-11 16:22:15 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { Overview } from "./components/overview/Overview";
 import { Sidebar } from "./components/sidebar/Sidebar";
 
 export default function App() {
-  return (<div className="w-full h-screen flex bg-Beige100">
-    <Sidebar />
-    <Overview />
+  return (
+    <div className="w-full h-screen flex bg-Beige100">
+      <Sidebar />
+      <div className="flex-grow overflow-y-auto">
+        <Overview />
+      </div>
     </div>
-  )
+  );
 }
